5a9087b58faefbc7d86902f01e70a57eef5e45a3
Siddharth Verma <88787415+vsiddharth365@users.noreply.github.com>
2022-11-06 00:47:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { CssBaseline } from '@mui/material';
 import './App.css';
-import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import CustomFooter from './pages/Footer';
 import Router from './Router';
 
 function App() {
